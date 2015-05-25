cb3fee47c27f7ffeb9724d1e68e8302434076812
Alexey Bannov <alexey.bannov@onlyoffice.com>
2015-05-25 05:52:26 -0400

    Update run_mailserver.sh

--- run_mailserver.sh
+++ run_mailserver.sh
@@ -2,6 +2,12 @@
 
 DATA_DIR="/usr/src/iRedMail-0.8.7"
 
+HTTPD_LOG_DIR="/var/log/httpd"
+
+if [ ! -f ${HTTPD_LOG_DIR} ]; then
+    mkdir ${HTTPD_LOG_DIR}
+fi
+
 # start services
 service crond start
 service mysqld start
